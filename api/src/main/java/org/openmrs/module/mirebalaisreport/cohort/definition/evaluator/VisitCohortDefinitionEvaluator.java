4e6d8d9668cb4e954ff5368bbff75be70cef41c7
djazayeri <jazayeri@alum.mit.edu>
2013-02-25 20:00:57 -0800

    #365 - Quick Win Report Implemented VisitCohortDefinition and its Evaluator

---
+++ api/src/main/java/org/openmrs/module/mirebalaisreport/cohort/definition/evaluator/VisitCohortDefinitionEvaluator.java
@@ -0,0 +1,105 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.mirebalaisreport.cohort.definition.evaluator;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.openmrs.Cohort;
+import org.openmrs.Visit;
+import org.openmrs.annotation.Handler;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.mirebalaisreport.cohort.definition.VisitCohortDefinition;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.evaluator.CohortDefinitionEvaluator;
+import org.openmrs.module.reporting.common.TimeQualifier;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Handler(supports = VisitCohortDefinition.class)
+public class VisitCohortDefinitionEvaluator implements CohortDefinitionEvaluator {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public EvaluatedCohort evaluate(CohortDefinition cohortDefinition, EvaluationContext context) throws EvaluationException {
+        VisitCohortDefinition cd = (VisitCohortDefinition) cohortDefinition;
+
+        if (cd.getTimeQualifier() != TimeQualifier.ANY) {
+            throw new IllegalArgumentException("Currently only timeQualifier=ANY is implemented");
+        }
+
+        // TODO transform onOrBefore to the last millisecond of the day if they have 00:00:00.000
+
+        Criteria crit = sessionFactory.getCurrentSession().createCriteria(Visit.class);
+        crit.add(Restrictions.eq("voided", false));
+        if (cd.getStartedOnOrAfter() != null) {
+            crit.add(Restrictions.ge("startDatetime", cd.getStartedOnOrAfter()));
+        }
+        if (cd.getStartedOnOrBefore() != null) {
+            crit.add(Restrictions.le("startDatetime", cd.getStartedOnOrBefore()));
+        }
+        if (cd.getStoppedOnOrAfter() != null) {
+            crit.add(Restrictions.ge("stopDatetime", cd.getStoppedOnOrAfter()));
+        }
+        if (cd.getStoppedOnOrBefore() != null) {
+            crit.add(Restrictions.le("stopDatetime", cd.getStoppedOnOrBefore()));
+        }
+        if (cd.getVisitTypeList() != null) {
+            crit.add(Restrictions.in("visitType", cd.getVisitTypeList()));
+        }
+        if (cd.getLocationList() != null) {
+            crit.add(Restrictions.in("location", cd.getLocationList()));
+        }
+        if (cd.getIndicationList() != null) {
+            crit.add(Restrictions.in("indication", cd.getIndicationList()));
+        }
+        if (cd.getCreatedBy() != null) {
+            crit.add(Restrictions.eq("creator", cd.getCreatedBy()));
+        }
+        if (cd.getCreatedOnOrAfter() != null) {
+            crit.add(Restrictions.ge("dateCreated", cd.getCreatedOnOrAfter()));
+        }
+        if (cd.getCreatedOnOrBefore() != null) {
+            crit.add(Restrictions.le("dateCreated", cd.getCreatedOnOrBefore()));
+        }
+        crit.setProjection(Projections.distinct(Projections.property("patient.id")));
+
+        Cohort c = new Cohort();
+        for (Integer i : (List<Integer>) crit.list()){
+            c.addMember(i);
+        }
+
+        if (cd.getReturnInverse()) {
+            Cohort baseCohort = context.getBaseCohort();
+            if (baseCohort == null) {
+                baseCohort = Context.getPatientSetService().getAllPatients();
+            }
+            c = Cohort.subtract(baseCohort, c);
+        }
+
+        return new EvaluatedCohort(c, cohortDefinition, context);
+    }
+
+}
