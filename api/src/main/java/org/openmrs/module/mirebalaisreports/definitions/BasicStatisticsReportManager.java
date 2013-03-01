ae3ec464aded43e91195babe985a1895f6ef6ac6
djazayeri <jazayeri@alum.mit.edu>
2013-02-28 20:41:01 -0800

    #365 - Quick Win Report Implemented Data Quality indicators

--- api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/BasicStatisticsReportManager.java
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/BasicStatisticsReportManager.java
@@ -1,10 +1,15 @@
 package org.openmrs.module.mirebalaisreports.definitions;
 
 import org.openmrs.EncounterType;
+import org.openmrs.Location;
+import org.openmrs.api.PatientSetService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.emr.EmrProperties;
 import org.openmrs.module.mirebalaisreports.cohort.definition.VisitCohortDefinition;
 import org.openmrs.module.patientregistration.PatientRegistrationGlobalProperties;
+import org.openmrs.module.reporting.cohort.definition.CodedObsCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.EncounterCohortDefinition;
 import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.module.reporting.dataset.DataSet;
@@ -18,41 +23,109 @@ import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.evaluation.parameter.Parameterizable;
 import org.openmrs.module.reporting.evaluation.parameter.ParameterizableUtil;
 import org.openmrs.module.reporting.indicator.CohortIndicator;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openmrs.ui.framework.SimpleObject;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class BasicStatisticsReportManager {
 
-    public BasicStatisticsReportManager() {
+    EmrProperties emrProperties;
+
+    public BasicStatisticsReportManager(EmrProperties emrProperties) {
+        this.emrProperties = emrProperties;
     }
 
     public MapDataSet evaluate(Date day) throws EvaluationException {
         day = DateUtil.getStartOfDay(day);
+
+        // metadata we're (hard)coding against
+
+        EncounterType registrationEncounterType = PatientRegistrationGlobalProperties.GLOBAL_PROPERTY_PATIENT_REGISTRATION_ENCOUNTER_TYPE();
+
+        EncounterType vitalsEncounterType = Context.getEncounterService().getEncounterTypeByUuid("4fb47712-34a6-40d2-8ed3-e153abbd25b7");
+        if (vitalsEncounterType == null) {
+            throw new IllegalStateException("Cannot find Vitals encounter type by uuid 4fb47712-34a6-40d2-8ed3-e153abbd25b7");
+        }
+
+        List<EncounterType> nonRegistrationEncounterTypes = Context.getEncounterService().getAllEncounterTypes(false);
+        nonRegistrationEncounterTypes.remove(registrationEncounterType);
+
+        Location outpatientClinic = Context.getLocationService().getLocationByUuid("199e7d87-92a0-4398-a0f8-11d012178164");
+        if (outpatientClinic == null) {
+            throw new IllegalStateException("Cannot find outpatient clinic by uuid 199e7d87-92a0-4398-a0f8-11d012178164");
+        }
 
         // set up underlying queries
         VisitCohortDefinition visitsStartedOnDayQuery = new VisitCohortDefinition();
         visitsStartedOnDayQuery.addParameter(new Parameter("startedOnOrAfter", "Start of Day", Date.class));
         visitsStartedOnDayQuery.addParameter(new Parameter("startedOnOrBefore", "End of Day", Date.class));
 
+        VisitCohortDefinition activeVisitsQuery = new VisitCohortDefinition();
+        activeVisitsQuery.setActive(true);
+
+        EncounterCohortDefinition todayRegistrationsQuery = new EncounterCohortDefinition();
+        todayRegistrationsQuery.setEncounterTypeList(Arrays.asList(registrationEncounterType));
+        todayRegistrationsQuery.setOnOrAfter(day);
+        todayRegistrationsQuery.setOnOrBefore(DateUtil.getEndOfDay(day));
+
+        EncounterCohortDefinition outpatientEncountersOnDayQuery = new EncounterCohortDefinition();
+        outpatientEncountersOnDayQuery.setLocationList(Arrays.asList(outpatientClinic));
+        outpatientEncountersOnDayQuery.setEncounterTypeList(nonRegistrationEncounterTypes);
+        outpatientEncountersOnDayQuery.addParameter(new Parameter("onOrAfter", "Start of day", Date.class));
+        outpatientEncountersOnDayQuery.addParameter(new Parameter("onOrBefore", "End of day", Date.class));
+
+        EncounterCohortDefinition encountersOfTypeInPeriodQuery = new EncounterCohortDefinition();
+        encountersOfTypeInPeriodQuery.addParameter(new Parameter("encounterTypeList", "Encounter Types", EncounterType.class));
+        encountersOfTypeInPeriodQuery.addParameter(new Parameter("onOrAfter", "Start of day", Date.class));
+        encountersOfTypeInPeriodQuery.addParameter(new Parameter("onOrBefore", "End of day", Date.class));
+
+        // not really a coded obs, but there's no specific ObsCohortDefinition for searching for groups, and this will work if we don't set a value constraint
+        CodedObsCohortDefinition diagnosisInPeriodQuery = new CodedObsCohortDefinition();
+        diagnosisInPeriodQuery.addParameter(new Parameter("onOrAfter", "Start of day", Date.class));
+        diagnosisInPeriodQuery.addParameter(new Parameter("onOrBefore", "End of day", Date.class));
+        diagnosisInPeriodQuery.setTimeModifier(PatientSetService.TimeModifier.ANY);
+        diagnosisInPeriodQuery.setQuestion(emrProperties.getDiagnosisMetadata().getDiagnosisSetConcept());
+
+        CompositionCohortDefinition diagnosisAndVitalsOnDayQuery = new CompositionCohortDefinition();
+        diagnosisAndVitalsOnDayQuery.addParameter(new Parameter("day", "Day", Date.class));
+        diagnosisAndVitalsOnDayQuery.addSearch("vitals", encountersOfTypeInPeriodQuery, SimpleObject.create("onOrAfter", "${day}", "onOrBefore", "${day}", "encounterTypeList", vitalsEncounterType));
+        diagnosisAndVitalsOnDayQuery.addSearch("diagnosis", map((CohortDefinition) diagnosisInPeriodQuery, "onOrAfter=${day},onOrBefore=${day}"));
+        diagnosisAndVitalsOnDayQuery.setCompositionString("vitals AND diagnosis");
+
         // set up indicators
-        CohortIndicator visitsStartedOnDay = new CohortIndicator();
+        CohortIndicator visitsStartedOnDay = new CohortIndicator("Visits Started on Day");
         visitsStartedOnDay.addParameter(new Parameter("day", "Day", Date.class));
         visitsStartedOnDay.setCohortDefinition(visitsStartedOnDayQuery, "startedOnOrAfter=${day},startedOnOrBefore=${day}");
 
-        VisitCohortDefinition activeVisitsQuery = new VisitCohortDefinition();
-        activeVisitsQuery.setActive(true);
-        CohortIndicator activeVisits = new CohortIndicator();
+        CohortIndicator activeVisits = new CohortIndicator("Active Visits");
         activeVisits.setCohortDefinition(activeVisitsQuery, "");
 
-        EncounterType encounterType = PatientRegistrationGlobalProperties.GLOBAL_PROPERTY_PATIENT_REGISTRATION_ENCOUNTER_TYPE();
-        EncounterCohortDefinition todayRegistrationsQuery = new EncounterCohortDefinition();
-        todayRegistrationsQuery.setEncounterTypeList(Arrays.asList(encounterType));
-        todayRegistrationsQuery.setOnOrAfter(day);
-        todayRegistrationsQuery.setOnOrBefore(DateUtil.getEndOfDay(day));
-        CohortIndicator todayRegistrations = new CohortIndicator();
+        CohortIndicator todayRegistrations = new CohortIndicator("Registrations Today");
         todayRegistrations.setCohortDefinition(todayRegistrationsQuery, "");
+
+        CohortIndicator outpatientEncountersOnDay = new CohortIndicator("Outpatients on Day");
+        outpatientEncountersOnDay.addParameter(new Parameter("day", "Day", Date.class));
+        outpatientEncountersOnDay.setCohortDefinition(outpatientEncountersOnDayQuery, "onOrAfter=${day},onOrBefore=${day}");
+
+        CohortIndicator outpatientEncountersOnDayWithVitals = new CohortIndicator("Outpatients on Day - % with vitals");
+        outpatientEncountersOnDayWithVitals.setType(CohortIndicator.IndicatorType.FRACTION);
+        outpatientEncountersOnDayWithVitals.addParameter(new Parameter("day", "Day", Date.class));
+        outpatientEncountersOnDayWithVitals.setCohortDefinition(encountersOfTypeInPeriodQuery, SimpleObject.create("onOrAfter", "${day}", "onOrBefore", "${day}", "encounterTypeList", vitalsEncounterType));
+        outpatientEncountersOnDayWithVitals.setDenominator(outpatientEncountersOnDayQuery, "onOrAfter=${day},onOrBefore=${day}");
+
+        CohortIndicator outpatientEncountersOnDayWithDiagnosis = new CohortIndicator("Outpatients on Day - % with diagnosis");
+        outpatientEncountersOnDayWithDiagnosis.setType(CohortIndicator.IndicatorType.FRACTION);
+        outpatientEncountersOnDayWithDiagnosis.addParameter(new Parameter("day", "Day", Date.class));
+        outpatientEncountersOnDayWithDiagnosis.setCohortDefinition(diagnosisInPeriodQuery, "onOrAfter=${day},onOrBefore=${day}");
+        outpatientEncountersOnDayWithDiagnosis.setDenominator(outpatientEncountersOnDayQuery, "onOrAfter=${day},onOrBefore=${day}");
+
+        CohortIndicator outpatientEncountersOnDayWithVitalsAndDiagnosis = new CohortIndicator("Outpatients on Day - $ with vitals and diagnosis");
+        outpatientEncountersOnDayWithVitalsAndDiagnosis.setType(CohortIndicator.IndicatorType.FRACTION);
+        outpatientEncountersOnDayWithVitalsAndDiagnosis.addParameter(new Parameter("day", "Day", Date.class));
+        outpatientEncountersOnDayWithVitalsAndDiagnosis.setCohortDefinition(diagnosisAndVitalsOnDayQuery, "day=${day}");
+        outpatientEncountersOnDayWithVitalsAndDiagnosis.setDenominator(outpatientEncountersOnDayQuery, "onOrAfter=${day},onOrBefore=${day}");
 
         // set up a dataset with the indicators
         CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
@@ -61,6 +134,10 @@ public class BasicStatisticsReportManager {
         dsd.addColumn("startedVisitDayBefore", "Started Visit On Day Before", map(visitsStartedOnDay, "day=${reportDay-1d}"), "");
         dsd.addColumn("activeVisits", "Current Active Visits", map(activeVisits, ""), "");
         dsd.addColumn("todayRegistrations", "Registrations made today", map(todayRegistrations, ""), "");
+        dsd.addColumn("outpatientsDayBefore", "Yesterday's Outpatients", map(outpatientEncountersOnDay, "day=${reportDay-1d}"), "");
+        dsd.addColumn("outpatientsDayBeforeWithVitals", "Yesterday's Outpatients with vitals", map(outpatientEncountersOnDayWithVitals, "day=${reportDay-1d}"), "");
+        dsd.addColumn("outpatientsDayBeforeWithDiagnosis", "Yesterday's Outpatients with diagnosis", map(outpatientEncountersOnDayWithDiagnosis, "day=${reportDay-1d}"), "");
+        dsd.addColumn("outpatientsDayBeforeWithVitalsAndDiagnosis", "Yesterday's Outpatients with diagnosis and vitals", map(outpatientEncountersOnDayWithVitalsAndDiagnosis, "day=${reportDay-1d}"), "");
 
         EvaluationContext evaluationContext = new EvaluationContext();
         evaluationContext.addParameterValue("reportDay", day);
--- api/src/test/java/org/openmrs/module/mirebalaisreports/definitions/BasicStatisticsReportManagerTest.java
+++ api/src/test/java/org/openmrs/module/mirebalaisreports/definitions/BasicStatisticsReportManagerTest.java
@@ -1,8 +1,13 @@
 package org.openmrs.module.mirebalaisreports.definitions;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.module.emr.EmrConstants;
+import org.mockito.ArgumentMatcher;
+import org.openmrs.Cohort;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.emr.EmrProperties;
+import org.openmrs.module.emr.consult.DiagnosisMetadata;
 import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.module.reporting.dataset.MapDataSet;
 import org.openmrs.module.reporting.indicator.dimension.CohortIndicatorAndDimensionResult;
@@ -10,8 +15,10 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 import java.util.Date;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BasicStatisticsReportManagerTest extends BaseModuleContextSensitiveTest {
 
@@ -24,7 +31,13 @@ public class BasicStatisticsReportManagerTest extends BaseModuleContextSensitive
     public void shouldBuildAndRunReport() throws Exception {
         Date day = DateUtil.parseDate("2012-01-02", "yyyy-MM-dd");
 
-        BasicStatisticsReportManager report = new BasicStatisticsReportManager();
+        DiagnosisMetadata dmd = new DiagnosisMetadata();
+        dmd.setDiagnosisSetConcept(Context.getConceptService().getConcept(23));
+
+        EmrProperties emrProperties = mock(EmrProperties.class);
+        when(emrProperties.getDiagnosisMetadata()).thenReturn(dmd);
+
+        BasicStatisticsReportManager report = new BasicStatisticsReportManager(emrProperties);
 
         MapDataSet result = report.evaluate(day);
 
@@ -32,11 +45,34 @@ public class BasicStatisticsReportManagerTest extends BaseModuleContextSensitive
         CohortIndicatorAndDimensionResult startedVisitDayBefore = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("startedVisitDayBefore");
         CohortIndicatorAndDimensionResult patientsCurrentlyInHospital = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("activeVisits");
         CohortIndicatorAndDimensionResult todayRegistrations = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("todayRegistrations");
+        CohortIndicatorAndDimensionResult outpatientsDayBefore = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("outpatientsDayBefore");
+        CohortIndicatorAndDimensionResult outpatientsDayBeforeWithVitals = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("outpatientsDayBeforeWithVitals");
+        CohortIndicatorAndDimensionResult outpatientsDayBeforeWithDiagnosis = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("outpatientsDayBeforeWithDiagnosis");
+        CohortIndicatorAndDimensionResult outpatientsDayBeforeWithVitalsAndDiagnosis = (CohortIndicatorAndDimensionResult) result.getData().getColumnValue("outpatientsDayBeforeWithVitalsAndDiagnosis");
 
-        assertThat(startedVisitOnDay.getValue().intValue(), is(3));
-        assertThat(startedVisitDayBefore.getValue().intValue(), is(2));
-        assertThat(patientsCurrentlyInHospital.getValue().intValue(), is(2));
-        assertThat(todayRegistrations.getValue().intValue(), is(1));
+        assertThat(startedVisitOnDay.getCohortIndicatorAndDimensionCohort(), hasCohort(2, 6, 7));
+        assertThat(startedVisitDayBefore.getCohortIndicatorAndDimensionCohort(), hasCohort(2, 7));
+        assertThat(patientsCurrentlyInHospital.getCohortIndicatorAndDimensionCohort(), hasCohort(2, 6));
+        assertThat(todayRegistrations.getCohortIndicatorAndDimensionCohort(), hasCohort(7));
+
+        Matcher<Cohort> isExpectedDenominatorCohort = hasCohort(2, 6, 7);
+        assertThat(outpatientsDayBefore.getCohortIndicatorAndDimensionCohort(), isExpectedDenominatorCohort);
+        assertThat(outpatientsDayBeforeWithVitals.getCohortIndicatorAndDimensionCohort(), hasCohort(2, 7));
+        assertThat(outpatientsDayBeforeWithVitals.getCohortIndicatorAndDimensionDenominator(), isExpectedDenominatorCohort);
+        assertThat(outpatientsDayBeforeWithDiagnosis.getCohortIndicatorAndDimensionCohort(), hasCohort(7));
+        assertThat(outpatientsDayBeforeWithDiagnosis.getCohortIndicatorAndDimensionDenominator(), isExpectedDenominatorCohort);
+        assertThat(outpatientsDayBeforeWithVitalsAndDiagnosis.getCohortIndicatorAndDimensionCohort(), hasCohort(7));
+        assertThat(outpatientsDayBeforeWithVitalsAndDiagnosis.getCohortIndicatorAndDimensionDenominator(), isExpectedDenominatorCohort);
+    }
+
+    private Matcher<Cohort> hasCohort(final Integer... expectedMemberIds) {
+        return new ArgumentMatcher<Cohort>() {
+            @Override
+            public boolean matches(Object argument) {
+                Cohort actual = (Cohort) argument;
+                return actual.size() == expectedMemberIds.length && containsInAnyOrder(expectedMemberIds).matches(actual.getMemberIds());
+            }
+        };
     }
 
 }
