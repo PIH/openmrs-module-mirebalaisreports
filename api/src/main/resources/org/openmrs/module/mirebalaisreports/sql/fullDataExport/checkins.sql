60753c31d76c4fb71e8872a083bac4edb51757ab
cospih <cioan@pih.org>
2013-08-30 10:38:02 -0400

    story#840, include check-in type_of_visit in full data export

--- api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/checkins.sql
+++ api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/checkins.sql
@@ -2,7 +2,7 @@ SELECT p.patient_id, zl.identifier zlemr, zl_loc.name loc_registered, un.value u
 
 										 --Mark as retrospective if more than 30 minutes elapsed between encounter date and creation
 IF(TIME_TO_SEC(e.date_created) - TIME_TO_SEC(e.encounter_datetime) > 1800, TRUE, FALSE) retrospective,
-reason_n.name reason_for_visit
+reason_n.name type_of_visit
 
 FROM patient p
 
@@ -37,7 +37,7 @@ INNER JOIN location el ON e.location_id = el.location_id
 --Payment amount
 LEFT OUTER JOIN obs pd ON e.encounter_id = pd.encounter_id AND pd.voided = 0 AND pd.concept_id = 124
 
---Reason for visit
+--Type of visit
 LEFT OUTER JOIN obs reason ON e.encounter_id = reason.encounter_id AND reason.voided = 0 AND reason.concept_id = 1244
 LEFT OUTER JOIN concept_name reason_n ON reason.value_coded = reason_n.concept_id AND reason_n.voided = 0 AND reason_n.locale = 'en' AND reason_n.concept_name_type = 'FULLY_SPECIFIED'
 
