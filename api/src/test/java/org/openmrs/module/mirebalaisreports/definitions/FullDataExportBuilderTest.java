0fd69784e62c60f0f0e3ba6091eb542b9d26117c
djazayeri <jazayeri@alum.mit.edu>
2013-09-30 14:53:14 -0400

    Adding a test I forgot to add to git before.

---
+++ api/src/test/java/org/openmrs/module/mirebalaisreports/definitions/FullDataExportBuilderTest.java
@@ -0,0 +1,43 @@
+package org.openmrs.module.mirebalaisreports.definitions;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+public class FullDataExportBuilderTest extends BaseMirebalaisReportTest {
+
+    @Autowired
+    FullDataExportBuilder builder;
+
+    @Test
+    public void shouldSetupTheReportWithAllDataSets() throws Exception {
+        FullDataExportBuilder.Configuration configuration = new FullDataExportBuilder.Configuration("uuid", "prefix", null);
+        ReportDefinition reportDefinition = builder.buildReportManager(configuration).constructReportDefinition();
+        Assert.assertEquals(13, reportDefinition.getDataSetDefinitions().size());
+    }
+
+    @Test
+    public void shouldSetupTheReportWithSelectedDataSets() throws Exception {
+        FullDataExportBuilder.Configuration configuration = new FullDataExportBuilder.Configuration("uuid", "prefix", Arrays.asList("patients", "visits"));
+        ReportDefinition reportDefinition = builder.buildReportManager(configuration).constructReportDefinition();
+        Assert.assertEquals(2, reportDefinition.getDataSetDefinitions().size());
+    }
+
+    @Test
+    public void shouldOrderTheDataSetsInTheReportCorrectly() throws Exception {
+        FullDataExportBuilder.Configuration configuration = new FullDataExportBuilder.Configuration("uuid", "prefix", null);
+        ReportDefinition reportDefinition = builder.buildReportManager(configuration).constructReportDefinition();
+        int i=0;
+        for (String dsName : reportDefinition.getDataSetDefinitions().keySet()) {
+			Assert.assertEquals(builder.dataSetOptions.get(i), dsName);
+            i++;
+        }
+    }
+
+}
