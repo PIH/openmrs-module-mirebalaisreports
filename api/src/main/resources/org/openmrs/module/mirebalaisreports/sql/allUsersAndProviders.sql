7e3d000d03c1509ddfbd22ba1397362164076734
Neissi Torres Lima <neissi.lima@gmail.com>
2013-12-19 13:56:19 -0200

    #988 - Updated query to return all users and prividers in the system

--- api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/allUsersAndProviders.sql
+++ api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/allUsersAndProviders.sql
@@ -1,9 +1,9 @@
-select distinct pe.given_name as 'Person name',
+select distinct concat(pe.given_name, ' ' ,pe.family_name) as 'Person name',
                 u.username as 'User name',
                 u.retired as 'User inactive',
-                u.date_created as 'Date created',
+                u.date_created as 'Date user account created',
                 (select group_concat(role) from user_role where user_id = u.user_id ) as 'Roles associated',
-                pp.name as 'Provider role associated'
+                pp.name as 'Provider type'
            from person_name pe
 
            inner join users u
