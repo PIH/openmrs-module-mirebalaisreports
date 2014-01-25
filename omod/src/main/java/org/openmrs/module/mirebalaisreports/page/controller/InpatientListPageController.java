282c1e40c4b847bd6b9731f6a00ed50890c0cb81
cioan <cioan123>
2014-01-25 13:29:04 -0500

    UHM-1010, switch to the new mirebalaisreports inpatient list page

--- omod/src/main/java/org/openmrs/module/mirebalaisreports/page/controller/InpatientListPageController.java
+++ omod/src/main/java/org/openmrs/module/mirebalaisreports/page/controller/InpatientListPageController.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Cohort;
 import org.openmrs.module.mirebalaisreports.MirebalaisReportsProperties;
+import org.openmrs.module.mirebalaisreports.cohort.definition.InpatientLocationCohortDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
 import org.openmrs.module.reporting.dataset.DataSet;
 import org.openmrs.module.reporting.dataset.DataSetRow;
@@ -29,35 +30,28 @@ public class InpatientListPageController {
     private final Log log = LogFactory.getLog(getClass());
 
     public void get(PageModel model,
-                                 @SpringBean ReportDefinitionService reportDefinitionService,
                                  @SpringBean AllDefinitionLibraries libraries,
                                  @SpringBean DataSetDefinitionService dsdService) throws EvaluationException {
         EvaluationContext context = new EvaluationContext();
         Date today = new Date();
         context.addParameterValue("day", today);
 
-        ReportDefinition reportDefinition = reportDefinitionService.getDefinitionByUuid(MirebalaisReportsProperties.INPATIENT_LIST_REPORT_DEFINITION_UUID);
-        ReportData data = reportDefinitionService.evaluate(reportDefinition, context);
-
-        MapDataSet cohortDataSet = (MapDataSet) data.getDataSets().get("cohorts");
-        CohortIndicatorAndDimensionResult censusAtStart = (CohortIndicatorAndDimensionResult) cohortDataSet.getData().getColumnValue("censusAtStart");
-        Cohort cohort = censusAtStart.getCohortIndicatorAndDimensionCohort();
         DataSet result = null ;
-        if (cohort != null){
-            PatientDataSetDefinition dsd = new PatientDataSetDefinition();
-            dsd.addColumn("patientId", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.patientId"), "");
-            dsd.addColumn("familyName", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.preferredName.familyName"), "");
-            dsd.addColumn("givenName", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.preferredName.givenName"), "");
-            dsd.addColumn("zlEmrId", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.mostRecentZlEmrId.identifier"), "");
-            dsd.addColumn("dossierNumber", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.mostRecentDossierNumber.identifier"), "");
-            dsd.addColumn("firstAdmittedLocation", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.admission.location"), "");
-            dsd.addColumn("admissionDateTime", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.admission.encounterDatetime"), "");
-            dsd.addColumn("inpatientLocation", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.inpatient.location"), "");
-            dsd.addColumn("inpatientDateTime", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.inpatient.encounterDatetime"), "");
+        PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+        InpatientLocationCohortDefinition inpatientLocationCohortDefinition = new InpatientLocationCohortDefinition();
+        dsd.addRowFilter(inpatientLocationCohortDefinition ,null);
+        dsd.addColumn("patientId", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.patientId"), "");
+        dsd.addColumn("familyName", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.preferredName.familyName"), "");
+        dsd.addColumn("givenName", libraries.getDefinition(PatientDataDefinition.class, "reporting.library.patientDataDefinition.builtIn.preferredName.givenName"), "");
+        dsd.addColumn("zlEmrId", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.mostRecentZlEmrId.identifier"), "");
+        dsd.addColumn("dossierNumber", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.mostRecentDossierNumber.identifier"), "");
+        dsd.addColumn("firstAdmittedLocation", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.admission.location"), "");
+        dsd.addColumn("admissionDateTime", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.admission.encounterDatetime"), "");
+        dsd.addColumn("inpatientLocation", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.inpatient.location"), "");
+        dsd.addColumn("inpatientDateTime", libraries.getDefinition(PatientDataDefinition.class, "mirebalais.patientDataCalculation.inpatient.encounterDatetime"), "");
 
-            context.setBaseCohort(cohort);
-            result = dsdService.evaluate(dsd, context);
-        }
+        result = dsdService.evaluate(dsd, context);
+
         model.addAttribute("inpatientsList", simplify(result));
     }
 
