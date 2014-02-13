21218d5ff257124d14db51a763b20c05814433a5
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-02-13 10:40:03 -0800

    Go to a fixed version for FullDataExportReportManager since the tests passed.

--- api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/FullDataExportReportManager.java
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/FullDataExportReportManager.java
@@ -97,7 +97,7 @@ public class FullDataExportReportManager extends BaseMirebalaisReportManager {
 
     @Override
     public String getVersion() {
-        return "1.2-SNAPSHOT"; // last change: setting design property on CSV exports to blacklist non-latin-1 characters
+        return "1.2"; // last change: splitting provider from creator
     }
 
 	//***** INSTANCE METHODS
