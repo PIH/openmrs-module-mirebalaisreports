f9b3d2eb027d6a3def27af16c983e313d0ff015d
mogoodrich <mgoodrich@pih.org>
2014-05-06 15:47:46 -0400

    REPORTING-659: Move basic boilerplate Visit classes into Reporting module Added back in unimplemented HasPrimaryDiagnosisVisitQuery

--- api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/MorbidityRegisterReportManager.java
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/MorbidityRegisterReportManager.java
@@ -16,6 +16,7 @@ package org.openmrs.module.mirebalaisreports.definitions;
 
 import java.util.Date;
 
+import org.openmrs.module.mirebalaisreports.visit.definition.HasPrimaryDiagnosisVisitQuery;
 import org.openmrs.module.mirebalaisreports.visit.definition.VisitStartDateDataDefinition;
 import org.openmrs.module.reporting.dataset.DataSet;
 import org.openmrs.module.reporting.dataset.definition.VisitDataSetDefinition;
@@ -25,7 +26,6 @@ import org.openmrs.module.reporting.evaluation.EvaluationException;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.evaluation.parameter.Parameterizable;
 import org.openmrs.module.reporting.evaluation.parameter.ParameterizableUtil;
-import org.openmrs.module.reporting.query.visit.definition.HasPrimaryDiagnosisVisitQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
