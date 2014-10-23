29d2097699ec2bbe68502d342a255d48bb69c973
mogoodrich <mgoodrich@pih.org>
2014-10-23 14:58:09 -0400

    UHM-1630: Figure out why consultations exports are missing encounter ids Moved dispo_encounter_id to last column in consultation encouter

--- api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/FullDataExportReportManager.java
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/FullDataExportReportManager.java
@@ -104,7 +104,7 @@ public class FullDataExportReportManager extends BaseMirebalaisReportManager {
 
     @Override
     public String getVersion() {
-        return "1.8";
+        return "1.9";
     }
 
 	//***** INSTANCE METHODS
