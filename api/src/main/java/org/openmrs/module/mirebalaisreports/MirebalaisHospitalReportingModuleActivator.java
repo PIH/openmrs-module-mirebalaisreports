6df60bb67ab9aad362d499710a890c2a5915cd7f
mogoodrich <mgoodrich@pih.org>
2014-12-11 12:48:49 -0500

    UHM-1726: Daily Statistics Report is broken

--- api/src/main/java/org/openmrs/module/mirebalaisreports/MirebalaisHospitalReportingModuleActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/MirebalaisHospitalReportingModuleActivator.java
@@ -16,160 +16,24 @@ package org.openmrs.module.mirebalaisreports;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.AdministrationService;
-import org.openmrs.api.context.Context;
-import org.openmrs.api.db.SerializedObject;
-import org.openmrs.api.db.SerializedObjectDAO;
 import org.openmrs.module.BaseModuleActivator;
 import org.openmrs.module.ModuleActivator;
-import org.openmrs.module.mirebalaisreports.definitions.AllPatientsWithIdsReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.AppointmentsReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.FullDataExportBuilder;
-import org.openmrs.module.mirebalaisreports.definitions.InpatientListReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.InpatientStatsDailyReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.InpatientStatsMonthlyReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.ReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.UsersAndProvidersReportManager;
-import org.openmrs.module.mirebalaisreports.definitions.helper.DailyIndicatorByLocationReportDefinition;
-import org.openmrs.module.reporting.report.ReportDesign;
-import org.openmrs.module.reporting.report.definition.ReportDefinition;
-import org.openmrs.module.reporting.report.definition.service.ReportDefinitionService;
-import org.openmrs.module.reporting.report.service.ReportService;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
 public class MirebalaisHospitalReportingModuleActivator extends BaseModuleActivator {
-	
+
 	protected Log log = LogFactory.getLog(getClass());
-
-    private ReportService reportService;
-    private ReportDefinitionService reportDefinitionService;
-    private SerializedObjectDAO serializedObjectDAO;
-    private AdministrationService administrationService;
-
-    public void setReportService(ReportService reportService) {
-        this.reportService = reportService;
-    }
-
-    public void setReportDefinitionService(ReportDefinitionService reportDefinitionService) {
-        this.reportDefinitionService = reportDefinitionService;
-    }
-
-    public void setSerializedObjectDAO(SerializedObjectDAO serializedObjectDAO) {
-        this.serializedObjectDAO = serializedObjectDAO;
-    }
-
-    public void setAdministrationService(AdministrationService administrationService) {
-        this.administrationService = administrationService;
-    }
 
     /**
 	 * @see ModuleActivator#started()
 	 */
 	public void started() {
-        reportService = Context.getService(ReportService.class);
-        reportDefinitionService = Context.getService(ReportDefinitionService.class);
-        serializedObjectDAO = Context.getRegisteredComponents(SerializedObjectDAO.class).get(0);
-        administrationService = Context.getAdministrationService();
-
-        setupFullDataExports();
-        setupOtherReports();
-
         log.info("Mirebalais Hospital Reporting Module Module started");
 	}
 
-    /**
-     * Currently we require these to be white-listed, until we've gone through all ReportManagers, and ensured they are
-     * ready to be included here
-     */
-    private void setupOtherReports() {
-        setupReport(Context.getRegisteredComponents(AllPatientsWithIdsReportManager.class).get(0));
-        setupReport(Context.getRegisteredComponents(InpatientStatsDailyReportManager.class).get(0));
-        setupReport(Context.getRegisteredComponents(InpatientStatsMonthlyReportManager.class).get(0));
-        setupReport(Context.getRegisteredComponents(InpatientListReportManager.class).get(0));
-        setupReport(Context.getRegisteredComponents(UsersAndProvidersReportManager.class).get(0));
-        setupReport(Context.getRegisteredComponents(AppointmentsReportManager.class).get(0));
-        for (DailyIndicatorByLocationReportDefinition manager : Context.getRegisteredComponents(DailyIndicatorByLocationReportDefinition.class)) {
-            setupReport(manager);
-        }
-    }
 
-    private void setupFullDataExports() {
-        FullDataExportBuilder fullDataExportBuilder = Context.getRegisteredComponents(FullDataExportBuilder.class).get(0);
-        for (ReportManager manager : fullDataExportBuilder.getAllReportManagers()) {
-            setupReport(manager);
-        }
-    }
-
-    /**
-     * This is only public for testing
-     * @param manager
-     */
-    public void setupReport(ReportManager manager) {
-        if (alreadyAtLatestVersion(manager)) {
-            return;
-        }
-
-        ReportDefinition reportDefinition = manager.constructReportDefinition();
-
-        log.info("Saving new definition of " + reportDefinition.getName());
-
-        ReportDefinition existing = reportDefinitionService.getDefinitionByUuid(reportDefinition.getUuid());
-        if (existing != null) {
-            // we need to overwrite the existing, rather than purge-and-recreate, to avoid deleting old ReportRequests
-            log.debug("overwriting existing ReportDefinition");
-            reportDefinition.setId(existing.getId());
-            Context.evictFromSession(existing);
-        }
-        else {
-            // incompatible class changes for a serialized object could mean that getting the definition return null
-            // and some serialization error gets logged. In that case we want to overwrite the invalid serialized definition
-            SerializedObject invalidSerializedObject = serializedObjectDAO.getSerializedObjectByUuid(reportDefinition.getUuid());
-            if (invalidSerializedObject != null) {
-                reportDefinition.setId(invalidSerializedObject.getId());
-                Context.evictFromSession(invalidSerializedObject);
-            }
-//            serializedObjectDAO.purgeObject(invalidSerializedObject.getId());
-        }
-
-        reportDefinitionService.saveDefinition(reportDefinition);
-
-        // purging a ReportDesign doesn't trigger any extra logic, so we can just purge-and-recreate here
-        List<ReportDesign> existingDesigns = reportService.getReportDesigns(reportDefinition, null, true);
-        if (existingDesigns.size() > 0) {
-            log.debug("Deleting " + existingDesigns.size() + " old designs for " + reportDefinition.getName());
-            for (ReportDesign design : existingDesigns) {
-                reportService.purgeReportDesign(design);
-            }
-        }
-
-        try {
-            List<ReportDesign> designs = manager.constructReportDesigns(reportDefinition);
-            for (ReportDesign design : designs) {
-                reportService.saveReportDesign(design);
-            }
-            administrationService.setGlobalProperty(globalPropertyFor(manager), manager.getVersion());
-        }
-        catch (IOException ex) {
-            log.error("Error constructing report design for " + reportDefinition.getName(), ex);
-        }
-    }
-
-    private boolean alreadyAtLatestVersion(ReportManager manager) {
-        String newVersion = manager.getVersion();
-        String existingVersion = administrationService.getGlobalProperty(globalPropertyFor(manager));
-        return existingVersion != null &&
-                existingVersion.equals(newVersion) &&
-                !newVersion.contains("-SNAPSHOT");
-    }
-
-    private String globalPropertyFor(ReportManager manager) {
-        return "mirebalaisreports." + manager.getUuid() + ".version";
-    }
 
     /**
 	 * @see ModuleActivator#stopped()
