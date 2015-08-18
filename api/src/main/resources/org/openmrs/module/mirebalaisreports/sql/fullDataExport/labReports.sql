787ca5f5b15bf6666b1101fcfc487deeececb348
mogoodrich <mgoodrich@pih.org>
2015-08-18 17:06:03 -0400

    tried to fix labResults sql by changing comments

--- api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/labReports.sql
+++ api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/labReports.sql
@@ -2,26 +2,25 @@ SELECT p.patient_id, zl.identifier zlemr, zl_loc.name loc_registered, un.value u
 CONCAT(pn.given_name, ' ',pn.family_name) provider,
 obsjoins.*
 FROM patient p
-/*Most recent ZL EMR ID*/
+-- Most recent ZL EMR ID
 INNER JOIN (SELECT patient_id, identifier, location_id FROM patient_identifier WHERE identifier_type = :zlId
             AND voided = 0 AND preferred = 1 ORDER BY date_created DESC) zl ON p.patient_id = zl.patient_id
-/*ZL EMR ID location*/
+-- ZL EMR ID location
 INNER JOIN location zl_loc ON zl.location_id = zl_loc.location_id
-/*--Unknown patient*/
-LEFT OUTER JOIN person_attribute un ON p.patient_id = un.person_id AND un.person_attribute_type_id =:unknownPt 
-            AND un.voided = 0
-/*--Gender*/
+-- Unknown patient
+LEFT OUTER JOIN person_attribute un ON p.patient_id = un.person_id AND un.person_attribute_type_id =:unknownPt AND un.voided = 0
+--Gender
 INNER JOIN person pr ON p.patient_id = pr.person_id AND pr.voided = 0
-/*-- Most recent address*/
+-- Most recent address
 LEFT OUTER JOIN (SELECT * FROM person_address WHERE voided = 0 ORDER BY date_created DESC) pa ON p.patient_id = pa.person_id
 INNER JOIN (SELECT person_id, given_name, family_name FROM person_name WHERE voided = 0 ORDER BY date_created desc) n ON p.patient_id = n.person_id
 INNER JOIN encounter e ON p.patient_id = e.patient_id and e.voided = 0 AND e.encounter_type = :labResultEnc
 INNER JOIN location el ON e.location_id = el.location_id
-/* Provider Name */
+--Provider Name
 INNER JOIN encounter_provider ep ON ep.encounter_id = e.encounter_id and ep.voided = 0
 INNER JOIN provider pv ON pv.provider_id = ep.provider_id 
 INNER JOIN person_name pn ON pn.person_id = pv.person_id and pn.voided = 0
-/*Straight Obs Joins*/
+--Straight Obs Joins
 INNER JOIN 
 (select o.encounter_id,
 max(CASE when crs.name = 'PIH' and crt.code = 'HEMOGLOBIN' then o.value_numeric end) 'Hemoglobin',
@@ -62,18 +61,18 @@ max(CASE when crs.name = 'PIH' and crt.code = 'LACTATE DEHYDROGENASE' then date(
 max(CASE when crs.name = 'PIH' and crt.code = 'INTERNATIONAL NORMALIZED RATIO' then o.value_numeric end) 'International_Normalized_Ratio',
 max(CASE when crs.name = 'PIH' and crt.code = 'INTERNATIONAL NORMALIZED RATIO' then date(o.obs_datetime) end) 'Date_International_Normalized_Ratio'
 from encounter e, concept_reference_map crm,  concept_reference_term crt, concept_reference_source crs, obs o
-/*joining in concept name to decode coded answers*/
+--joining in concept name to decode coded answers
 LEFT OUTER JOIN concept_name cn on o.value_coded = cn.concept_id and cn.locale = 'en' and cn.locale_preferred = '1'  and cn.voided = 0
-/*joining in ob2 table second time for obsgroups*/
+--joining in ob2 table second time for obsgroups
 LEFT OUTER JOIN obs obs2 on obs2.obs_id = o.obs_group_id
-/*joining in this subquery for when stored in obsgroups*/ 
+--joining in this subquery for when stored in obsgroups
 LEFT OUTER JOIN 
 (select crm2.concept_id,crs2.name, crt2.code from concept_reference_map crm2, concept_reference_term crt2, concept_reference_source crs2
 where 1=1
 and crm2.concept_reference_term_id = crt2.concept_reference_term_id 
 and crt2.concept_source_id = crs2.concept_source_id) obsgrp on obsgrp.concept_id = obs2.concept_id
 where 1=1 
-and e.encounter_type= :labResultEnc /* including this in subquery should make the query faster*/
+and e.encounter_type= :labResultEnc --including this in subquery should make the query faster
 and crm.concept_reference_term_id = crt.concept_reference_term_id
 and crt.concept_source_id = crs.concept_source_id
 and crm.concept_id = o.concept_id
@@ -82,9 +81,9 @@ and e.voided = 0
 and o.voided = 0
 group by o.encounter_id
 ) obsjoins ON obsjoins.encounter_id = ep.encounter_id
-/* end columns joins */
+--end columns joins
 WHERE p.voided = 0
-/*exclude test patients*/
+--exclude test patients
 AND p.patient_id NOT IN (SELECT person_id FROM person_attribute WHERE value = 'true' AND person_attribute_type_id = :testPt 
                          AND voided = 0)
 AND date(e.encounter_datetime) >= :startDate 
