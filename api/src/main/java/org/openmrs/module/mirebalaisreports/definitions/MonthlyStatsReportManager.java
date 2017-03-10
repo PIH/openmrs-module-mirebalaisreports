b27ec1c710c3bd24cf32f25d0cb84da3c0d8d81c
mgoodrich <mark.goodrich@live.com>
2017-03-10 16:18:07 -0500

    initial commit of monthly stats report

---
+++ api/src/main/java/org/openmrs/module/mirebalaisreports/definitions/MonthlyStatsReportManager.java
@@ -0,0 +1,72 @@
+package org.openmrs.module.mirebalaisreports.definitions;
+
+import org.openmrs.module.mirebalaisreports.MirebalaisReportsProperties;
+import org.openmrs.module.pihcore.config.ConfigDescriptor;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.openmrs.module.mirebalaisreports.definitions.BaseReportManager.Category.MONITORING;
+
+@Component
+public class MonthlyStatsReportManager extends BaseMirebalaisReportManager {
+
+    private static final String EXCEL_TEMPLATE_NAME = "MonthlyStatsReportTemplate";
+
+    private static final String REPEATING_SECTION = "sheet:1,row:7,dataset:monthlyStats";
+
+    @Override
+    public BaseReportManager.Category getCategory() {
+        return MONITORING;
+    }
+
+    @Override
+    public List<ConfigDescriptor.Country> getCountries() {
+        return Arrays.asList(ConfigDescriptor.Country.HAITI);
+    }
+
+    @Override
+    public Integer getOrder() {
+        return REPORTING_MONITORING_REPORTS_ORDER.indexOf(getUuid());
+
+    }
+
+    @Override
+    public String getName() {
+        return "monthlyStats";
+    }
+
+    @Override
+    public String getUuid() {
+        return MirebalaisReportsProperties.MONTHLY_STATS_REPORTING_DEFINITION_UUID;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0-SNAPSHOT";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        return getStartAndEndDateParameters();
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+        return constructSqlReportDefinition(getName());
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) throws IOException {
+        return Arrays.asList(xlsReportDesign(reportDefinition, EXCEL_TEMPLATE_NAME, REPEATING_SECTION));
+    }
+
+
+
+
+}
