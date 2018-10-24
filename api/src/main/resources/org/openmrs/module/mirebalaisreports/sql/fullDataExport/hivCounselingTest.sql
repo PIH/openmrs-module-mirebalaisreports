b36a09507a70c99fd89c3e0b7f2e1cf057528fda
James Mbabazi <jmbabazi.pih.org>
2018-10-24 10:49:44 -0400

    These are the changes we've made: Changing the way the encounter is brought in (using parameter for Encounter Type passed in through UI) :vctEnc Commenting each join Add Encounter Date Change mother's first name join to be LEFT instead of INNER, since some patients were registered before this was required. order by Encounter Date

--- api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/hivCounselingTest.sql
+++ api/src/main/resources/org/openmrs/module/mirebalaisreports/sql/fullDataExport/hivCounselingTest.sql
@@ -21,6 +21,7 @@ SELECT
               MOD(TIMESTAMPDIFF(MONTH, birthdate, NOW()),
                   12))
        AS CHAR)                        AS AGE,
+  DATE(e.encounter_datetime)           AS 'ENCOUNTER DATE',     
   category_hiv_screen.result           AS 'MOTIF DE DEPISTAGE (#1 - #8)',
   reason_hiv_screen.notes              AS 'NOTES',
   (SELECT name
@@ -65,20 +66,18 @@ FROM
   encounter e
   INNER JOIN
   current_name_address cna ON cna.person_id = e.patient_id
-                              AND date(e.encounter_datetime) >= :startDate
-                              AND date(e.encounter_datetime) <= :endDate
-                              AND e.form_id = (SELECT form_id
-                                               FROM
-                                                 form
-                                               WHERE
-                                                 uuid = '616b6b36-f189-11e7-8c3f-9a214cf093ae')
-  INNER JOIN
+							  AND date(e.encounter_datetime) >= :startDate
+							  AND date(e.encounter_datetime) <= :endDate
+                              AND e.encounter_type = :vctEnc
+  -- Name of Mother                                               
+  LEFT OUTER JOIN
   person_attribute pa ON pa.person_id = e.patient_id
                          AND pa.person_attribute_type_id = (SELECT person_attribute_type_id
                                                             FROM
                                                               person_attribute_type
                                                             WHERE
                                                               name = 'First Name of Mother')
+  -- Category of HIV screening 															
   LEFT JOIN
   (SELECT
      person_id,
@@ -98,6 +97,7 @@ FROM
                                          rm.source = 'CIEL' AND rm.code = 164082)
    GROUP BY o.encounter_id) category_hiv_screen ON category_hiv_screen.person_id = e.patient_id
                                                    AND e.encounter_id = category_hiv_screen.encounter_id
+  -- Reason for HIV screening note
   LEFT JOIN
   (SELECT
      person_id,
@@ -117,6 +117,7 @@ FROM
                                          rm.source = 'PIH' AND rm.code = 11535)
    GROUP BY o.encounter_id) reason_hiv_screen ON reason_hiv_screen.person_id = e.patient_id
                                                  AND e.encounter_id = reason_hiv_screen.encounter_id
+  -- Victim of gender-based violence
   LEFT JOIN
   obs o ON o.person_id = e.patient_id
            AND o.concept_id = (SELECT concept_id
@@ -131,6 +132,7 @@ FROM
                                 rm.source = 'CIEL' AND rm.code = 165088)
            AND o.voided = 0
            AND e.encounter_id = o.encounter_id
+  -- Date of pre-test counseling
   LEFT JOIN
   obs prcd ON prcd.person_id = e.patient_id
               AND prcd.concept_id = (SELECT concept_id
@@ -140,6 +142,7 @@ FROM
                                        rm.source = 'PIH' AND rm.code = 11577)
               AND prcd.voided = 0
               AND e.encounter_id = prcd.encounter_id
+  -- HIV test date
   LEFT JOIN
   obs hivd ON hivd.person_id = e.patient_id
               AND hivd.concept_id = (SELECT concept_id
@@ -149,6 +152,7 @@ FROM
                                        rm.source = 'CIEL' AND rm.code = 164400)
               AND hivd.voided = 0
               AND e.encounter_id = hivd.encounter_id
+  -- HIV rapid test, qualitative
   LEFT JOIN
   obs hivr ON hivr.person_id = e.patient_id
               AND hivr.concept_id = (SELECT concept_id
@@ -158,6 +162,7 @@ FROM
                                        rm.source = 'CIEL' AND rm.code = 163722)
               AND hivd.voided = 0
               AND e.encounter_id = hivr.encounter_id
+  -- HIV test result received date
   LEFT JOIN
   obs hivrd ON hivrd.person_id = e.patient_id
                AND hivrd.concept_id = (SELECT concept_id
@@ -167,6 +172,7 @@ FROM
                                          rm.source = 'CIEL' AND rm.code = 160082)
                AND hivrd.voided = 0
                AND e.encounter_id = hivrd.encounter_id
+  -- Date of laboratory test should be in obs_group_id of RPR test construct
   LEFT JOIN
   obs rprd ON rprd.person_id = e.patient_id
               AND rprd.concept_id = (SELECT concept_id
@@ -186,6 +192,7 @@ FROM
                                                         WHERE
                                                           rm.source = 'PIH' AND rm.code = 11523)
                                           AND voided = 0)
+  -- Rapid Plasma Reagin value should be in obs_group_id of RPR test construct
   LEFT JOIN
   obs rpr ON rpr.person_id = e.patient_id
              AND rpr.concept_id = (SELECT concept_id
@@ -205,6 +212,7 @@ FROM
                                                       WHERE
                                                         rm.source = 'PIH' AND rm.code = 11523)
                                         AND voided = 0)
+  -- Date of test results should be in obs_group_id of RPR test construct
   LEFT JOIN
   obs rprrd ON rprrd.person_id = e.patient_id
                AND rprrd.concept_id = (SELECT concept_id
@@ -224,6 +232,7 @@ FROM
                                                           WHERE
                                                             rm.source = 'PIH' AND rm.code = 11523)
                                             AND voided = 0)
+  -- Date of post-test counseling
   LEFT JOIN
   obs postd ON postd.person_id = e.patient_id
                AND postd.concept_id = (SELECT concept_id
@@ -233,6 +242,7 @@ FROM
                                          rm.source = 'PIH' AND rm.code = 11525)
                AND postd.voided = 0
                AND e.encounter_id = postd.encounter_id
+  -- Tuberculosis evaluation (if yes, we get the obs_date)
   LEFT JOIN
   obs tbd ON tbd.person_id = e.patient_id
              AND tbd.concept_id = (SELECT concept_id
@@ -242,6 +252,7 @@ FROM
                                      rm.source = 'PIH' AND rm.code = 11541)
              AND tbd.voided = 0
              AND e.encounter_id = tbd.encounter_id
+  -- Tuberculosis symptom present 
   LEFT JOIN
   (SELECT
      person_id,
@@ -261,6 +272,7 @@ FROM
                                              rm.source = 'PIH' AND rm.code = 11563)
    GROUP BY otb.encounter_id) tb_evaluation ON tb_evaluation.person_id = e.patient_id
                                                AND e.encounter_id = tb_evaluation.encounter_id
+  -- RPR drug start date
   LEFT JOIN
   obs rxrpr ON rxrpr.person_id = e.patient_id
                AND rxrpr.concept_id = (SELECT concept_id
@@ -270,6 +282,7 @@ FROM
                                          rm.source = 'PIH' AND rm.code = 11536)
                AND rxrpr.voided = 0
                AND e.encounter_id = rxrpr.encounter_id
+  -- RPR drug end date
   LEFT JOIN
   obs rxrprd ON rxrprd.person_id = e.patient_id
                 AND rxrprd.concept_id = (SELECT concept_id
@@ -279,6 +292,7 @@ FROM
                                            rm.source = 'PIH' AND rm.code = 11537)
                 AND rxrprd.voided = 0
                 AND e.encounter_id = rxrprd.encounter_id
+  -- Date referred to HIV service
   LEFT JOIN
   obs rspec ON rspec.person_id = e.patient_id
                AND rspec.concept_id = (SELECT concept_id
@@ -288,6 +302,7 @@ FROM
                                          rm.source = 'PIH' AND rm.code = 11538)
                AND rspec.voided = 0
                AND e.encounter_id = rspec.encounter_id
+  -- Start date of HIV drug for blood exposure
   LEFT JOIN
   obs pres ON pres.person_id = e.patient_id
               AND pres.concept_id = (SELECT concept_id
@@ -297,6 +312,7 @@ FROM
                                        rm.source = 'PIH' AND rm.code = 11539)
               AND pres.voided = 0
               AND e.encounter_id = pres.encounter_id
+  -- Start date of HIV drug for sexual assault
   LEFT JOIN
   obs preas ON preas.person_id = e.patient_id
                AND preas.concept_id = (SELECT concept_id
@@ -306,6 +322,7 @@ FROM
                                          rm.source = 'PIH' AND rm.code = 11540)
                AND preas.voided = 0
                AND e.encounter_id = preas.encounter_id
+  -- Clinical management plan comment
   LEFT JOIN
   obs cmnt ON cmnt.person_id = e.patient_id
               AND cmnt.concept_id = (SELECT concept_id
@@ -315,4 +332,4 @@ FROM
                                        rm.source = 'CIEL' AND rm.code = 162749)
               AND cmnt.voided = 0
               AND e.encounter_id = cmnt.encounter_id
-ORDER BY cna.family_name;
+ORDER BY e.encounter_datetime;
